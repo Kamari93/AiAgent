7fdb259a9190b8f9b4f1a6253acd785415cab15b
Kamari93 <kmoore971@gmail.com>
2026-02-28 10:03:59 -0600

    B: Added prompt and response token count display

--- main.py
+++ main.py
@@ -14,11 +14,19 @@ client = genai.Client(api_key=api_key)
 
 def main():
     print("Hello from aiagent!")
+    user_prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
     response = client.models.generate_content(
-            model="gemini-2.5-flash", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+            model="gemini-2.5-flash", contents=user_prompt
         )
+    
+    if response.usage_metadata is None:
+         raise RuntimeError("Usage metadata is not available in the response.")
+    print(f"User Prompt: {user_prompt}")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
     ## the generate_content method returns a response object that contains the generated content in the text attribute
-    print(response.text)
+    print(f'Response:\n{response.text}')
 
 
 if __name__ == "__main__":
