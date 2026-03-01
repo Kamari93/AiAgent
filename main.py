c190a1e92d08d0772b87c464e6c1790af43e3ae5
Kamari93 <kmoore971@gmail.com>
2026-03-01 07:26:29 -0600

    C: Added command line argument with argparse for user_input

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
+import argparse
 from dotenv import load_dotenv
 from google import genai
+
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -14,14 +16,17 @@ client = genai.Client(api_key=api_key)
 
 def main():
     print("Hello from aiagent!")
-    user_prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    # user_prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    parser = argparse.ArgumentParser(description="Aiagent")
+    parser.add_argument("user_prompt", type=str, help="The prompt to send to the Gemini API")
+    args = parser.parse_args()
     response = client.models.generate_content(
-            model="gemini-2.5-flash", contents=user_prompt
+            model="gemini-2.5-flash", contents=args.user_prompt
         )
     
     if response.usage_metadata is None:
          raise RuntimeError("Usage metadata is not available in the response.")
-    print(f"User Prompt: {user_prompt}")
+    print(f"User Prompt: {args.user_prompt}")
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
